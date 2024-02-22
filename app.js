29553c24431648463b1d0e957273fcad918a4aea
Mizna Masood <miznamasood2007@gmail.com>
2024-02-22 01:45:42 -0800

    Assigment pdf 4

---
+++ app.js
@@ -0,0 +1,90 @@
+//  Chap 1 ___________-
+//  Q1
+
+var firstname = "Jhon";
+var lastname = "Doe";
+var email = "jhondoe@205.com";
+var phonenumber = "0385076748";
+var password = "456789";
+
+alert("firstname: " + firstname);
+alert("lastname: " + lastname);
+
+//  Q2
+
+alert("you're learning javascript!");
+
+//  Q3
+
+alert("Hello World");
+
+//  Chap 2 _______-
+
+//  Q1
+
+var myName = "Mizna";
+
+//  Q2
+
+var favouriteColour;
+
+var favouriteColour = "red"
+
+//  Q3
+
+var teamName = "Champions"
+
+alert(teamName)
+
+//  Q4
+
+var bestman = "Jhon"
+
+// Chap 3_________-
+
+// Q1
+
+var caseQty;
+
+// Q2
+
+var caseQty = 144
+
+// Q3
+
+var num = 9
+
+//  Q4
+
+var sum;
+
+var sum = 2 + 3;
+
+console.log(sum)
+
+// Q5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+
+var orderTotal = merchTotal + shippingCharge;
+
+ console.log(orderTotal)
+
+//  output 110 Ans..
+
+// Q6
+
+var number = 5;
+var number2 = number + 3;
+
+console.log(number2)
+
+// __________________
+
+
+
+
+
+
+
